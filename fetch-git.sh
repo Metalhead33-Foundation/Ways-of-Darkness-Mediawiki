cd7dd7f31903903b639c92bda9747cfb3e3636a7
Daniel Sonck <daniel@sonck.nl>
2020-02-29 21:56:25 +0100

    Improve error checking in fetch-git.sh

--- fetch-git.sh
+++ fetch-git.sh
@@ -9,34 +9,34 @@ MW_EXTENSIONS="BlockAndNuke VoteNY SocialProfile WikiTextLoggedInOut BlogPage Ca
 
 MW_SKINS="Vector Timeless Metrolook"
 
-mkdir -p addons &&
-pushd addons &&
+mkdir -p addons  || exit 1
+pushd addons  || exit 1
 
-mkdir -p extensions &&
-pushd extensions &&
-echo "Downloading extensions!" &&
+mkdir -p extensions  || exit 1
+pushd extensions  || exit 1
+echo "Downloading extensions!"
 for MW in ${MW_EXTENSIONS}
 do
-    echo "Downloading ${MW}" &&
-    [ -e ${MW} ] || git clone --recursive https://github.com/wikimedia/mediawiki-extensions-${MW}.git ${MW} -b REL${WIKI_RELEASE} &&
-done &&
-[ -e NativeSvgHandler ] || git clone --recursive https://github.com/wikimedia/mediawiki-extensions-NativeSvgHandler.git NativeSvgHandler &&
-[ -e EmbedVideo ] || git clone --recursive https://gitlab.com/hydrawiki/extensions/EmbedVideo.git &&
-[ -e SimpleMathJax ] || git clone --recursive https://github.com/jmnote/SimpleMathJax.git &&
-[ -e 3DAlloy ] || git clone --recursive https://github.com/dolfinus/3DAlloy &&
-popd &&
+    echo "Downloading ${MW}"
+    [ -e ${MW} ] || git clone --recursive https://github.com/wikimedia/mediawiki-extensions-${MW}.git ${MW} -b REL${WIKI_RELEASE} || exit 1
+done
+[ -e NativeSvgHandler ] || git clone --recursive https://github.com/wikimedia/mediawiki-extensions-NativeSvgHandler.git NativeSvgHandler || exit 1
+[ -e EmbedVideo ] || git clone --recursive https://gitlab.com/hydrawiki/extensions/EmbedVideo.git || exit 1
+[ -e SimpleMathJax ] || git clone --recursive https://github.com/jmnote/SimpleMathJax.git || exit 1
+[ -e 3DAlloy ] || git clone --recursive https://github.com/dolfinus/3DAlloy || exit 1
+popd
 
-mkdir -p skins &&
-pushd skins &&
-echo "Downloading skins!" &&
+mkdir -p skins  || exit 1
+pushd skins  || exit 1
+echo "Downloading skins!"
 for MW in ${MW_SKINS}
 do
-    [ -e ${MW} ] || git clone --recursive https://github.com/wikimedia/mediawiki-skins-${MW}.git ${MW} -b REL${WIKI_RELEASE} &&
-done &&
-[ -e Tweeki ] || git clone --recursive https://github.com/thaider/Tweeki.git &&
-[ -e foreground ] || git  clone --recursive https://github.com/jthingelstad/foreground.git &&
-[ -e chameleon ] || git  clone --recursive https://github.com/cmln/chameleon.git &&
-[ -e pivot ] || git clone --recursive https://github.com/Hutchy68/pivot.git &&
-popd &&
+    [ -e ${MW} ] || git clone --recursive https://github.com/wikimedia/mediawiki-skins-${MW}.git ${MW} -b REL${WIKI_RELEASE} || exit 1
+done
+[ -e Tweeki ] || git clone --recursive https://github.com/thaider/Tweeki.git || exit 1
+[ -e foreground ] || git  clone --recursive https://github.com/jthingelstad/foreground.git || exit 1
+[ -e chameleon ] || git  clone --recursive https://github.com/cmln/chameleon.git || exit 1
+[ -e pivot ] || git clone --recursive https://github.com/Hutchy68/pivot.git || exit 1
+popd
 
 popd
