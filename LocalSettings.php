9d6a525f4ccf04c454d50a10c7ec8d14c373704e
Zsolt Tóth <waysofdarkness@gmail.com>
2020-04-03 00:40:20 +0200

    Replaced some things relating to database credentials.

--- LocalSettings.php
+++ LocalSettings.php
@@ -61,14 +61,8 @@ $wgEmailAuthentication = true;
 
 ## Database settings
 $wgDBtype = "postgres";
-$wgDBserver = $_ENV['POSTGRES_HOST'];
-$wgDBname = $_ENV['POSTGRES_NAME'];
-$wgDBuser = $_ENV['POSTGRES_USER'];
-$wgDBpassword = $_ENV['POSTGRES_PASSWORD'];
-
-# Postgres specific settings
-$wgDBport = $_ENV['POSTGRES_PORT'];
 $wgDBmwschema = "mediawiki";
+require('/vault/secrets/db-cred.php');
 
 ## Shared memory settings
 $wgMainCacheType = CACHE_ACCEL;
