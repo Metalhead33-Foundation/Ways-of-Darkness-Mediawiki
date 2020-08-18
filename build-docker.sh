5eb247717de772eeeb4977abca99a4652a5d3715
Daniel Sonck <daniel@sonck.nl>
2020-08-19 00:20:59 +0200

    Revert buildah build

--- build-docker.sh
+++ build-docker.sh
@@ -44,36 +44,37 @@ run_composer() {
     done
 }
 
-C=$(buildah from "mediawiki:$MEDIAWIKI_VERSION")
+#C=$(buildah from "mediawiki:$MEDIAWIKI_VERSION")
 
-buildah run -- "${C}" usermod -u 1033 www-data || on_fail
-buildah run -- "${C}" apt-get update || on_fail
-buildah run -- "${C}" apt-get install -y lua5.1-dev libpq-dev unzip locales || on_fail
-buildah run --add-history -- "${C}" locale-gen --no-purge en_US.UTF-8 || on_fail
-buildah run -- "${C}" docker-php-source extract || on_fail
+#buildah run -- "${C}" usermod -u 1033 www-data || on_fail
+#buildah run -- "${C}" apt-get update || on_fail
+#buildah run -- "${C}" apt-get install -y lua5.1-dev libpq-dev unzip locales || on_fail
+#buildah run --add-history -- "${C}" locale-gen --no-purge en_US.UTF-8 || on_fail
+#buildah run -- "${C}" docker-php-source extract || on_fail
 
-install_pecl_extensions luasandbox
-install_extensions pdo pdo_pgsql pgsql
+#install_pecl_extensions luasandbox
+#install_extensions pdo pdo_pgsql pgsql
 
-buildah run --add-history -- "${C}" docker-php-source delete || on_fail
+#buildah run --add-history -- "${C}" docker-php-source delete || on_fail
 
-buildah copy ${C} fonts/ addons/ LocalSettings.php ./ || on_fail
-buildah copy ${C} htaccess .htaccess || on_fail
+#buildah copy ${C} fonts/ addons/ LocalSettings.php ./ || on_fail
+#buildah copy ${C} htaccess .htaccess || on_fail
 
-buildah copy ${C} "https://getcomposer.org/installer" composer-setup.php || on_fail
-buildah run -- "${C}" php composer-setup.php || on_fail
-buildah run -- "${C}" rm composer-setup.php || on_fail
-buildah run -- "${C}" php composer.phar install || on_fail
+#buildah copy ${C} "https://getcomposer.org/installer" composer-setup.php || on_fail
+#buildah run -- "${C}" php composer-setup.php || on_fail
+#buildah run -- "${C}" rm composer-setup.php || on_fail
+#buildah run -- "${C}" php composer.phar install || on_fail
 
-run_composer Widgets TemplateStyles
+#run_composer Widgets TemplateStyles
 
-buildah run -- "${C}" rm composer.phar || on_fail
+#buildah run -- "${C}" rm composer.phar || on_fail
 
-buildah run -- "${C}" chown www-data -R . || on_fail
+#buildah run -- "${C}" chown www-data -R . || on_fail
 
-buildah commit "${C}" "docker://$REPOSITORY"
+#buildah commit "${C}" "docker://$REPOSITORY"
 
-# podman --cgroup-manager=cgroupfs image build . -t $REPOSITORY || exit 1
-# podman --cgroup-manager=cgroupfs image push $REPOSITORY || exit 1
+buildah bud -t docker://$REPOSITORY . || exit 1
+buildah push docker://$REPOSITORY || exit 1
+#podman --cgroup-manager=cgroupfs image push $REPOSITORY || exit 1
 
 echo "version=$VERSION" > variables.txt
